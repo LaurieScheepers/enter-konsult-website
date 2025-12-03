2804229dcd68b8b8c82560f24cbe02c6d633510f
LaurieScheepers <laurscheepers@gmail.com>
2025-12-03 18:32:58 +0200

    Change INTERNAL to PRIVATE

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const ForwardEnterIcon = ({ className }) => (
     fill="currentColor"
     className={className}
   >
-    <polygon points="108.9 264.8 99.4 274.2 115.1 288 53.9 288 53.9 247 41.7 247 41.7 300.2 113.3 300.2 99.4 314.6 108.5 323.5 138.5 293.9 108.9 264.8"/>
+    <polygon points="108.9 264.8 99.4 274.2 115.1 288 53.9 288 53.9 247 41.7 247 41.7 300.2 113.3 300.2 99.4 314.6 108.5 323.5 138.5 293.9 108.9 264.8" />
   </svg>
 );
 
@@ -242,7 +242,7 @@ const ShowcasePage = () => (
                 <ExternalLink className="w-5 h-5" />
               </a>
             ) : (
-              <span className="font-mono text-xs text-gray-300 uppercase tracking-wide">Internal</span>
+              <span className="font-mono text-xs text-gray-300 uppercase tracking-wide">Private</span>
             )}
           </div>
         </div>
